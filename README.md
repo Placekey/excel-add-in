2b4c2a1089f621bb5ec94a86f998958c918d3363
Sandun Tharaka <spstharaka@gmail.com>
2020-11-26 23:27:18 +0530

    Add base of the Excel add-in

---
+++ README.md
@@ -0,0 +1,23 @@
+# Placekey Excel Add-in
+
+Placekey is a free, universal standard identifier for any physical place. This Google Sheets Add-on makes it easy to generate Placekeys for your address or POI data. 
+
+## Full Documentation
+https://docs.placekey.io/#cb3ca616-9939-4bef-a978-84f9895e31b3
+
+
+## Debugging
+
+Supports debugging using any of the following techniques:
+
+- [Use a browser's developer tools](https://docs.microsoft.com/office/dev/add-ins/testing/debug-add-ins-in-office-online)
+- [Attach a debugger from the task pane](https://docs.microsoft.com/office/dev/add-ins/testing/attach-debugger-from-task-pane)
+- [Use F12 developer tools on Windows 10](https://docs.microsoft.com/office/dev/add-ins/testing/debug-add-ins-using-f12-developer-tools-on-windows-10)
+
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[Apache-2.0 License](https://github.com/Placekey/excel-add-in/blob/main/LICENSE)
---
+++ src/taskpane/components/App.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import Header from "./Header";
+import Progress from "./Progress";
+// images references in the manifest
+import "../../../assets/ribbon-ico-16.png";
+import "../../../assets/ribbon-ico-32.png";
+import "../../../assets/ribbon-ico-80.png";
+/* global Button, console, Excel, Header, HeroList, HeroListItem, Progress */
+
+export interface AppProps {
+  title: string;
+  isOfficeInitialized: boolean;
+}
+
+export interface AppState {
+  
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  click = async () => {
+    try {
+      await Excel.run(async context => {
+        /**
+         * Insert your Excel code here
+         */
+        const range = context.workbook.getSelectedRange();
+
+        // Read the range address
+        range.load("address");
+
+        // Update the fill color
+        range.format.fill.color = "yellow";
+
+        await context.sync();
+        console.log(`The range address was ${range.address}.`);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    const { title, isOfficeInitialized } = this.props;
+
+    if (!isOfficeInitialized) {
+      return (
+        <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body." />
+      );
+    }
+
+    return (
+      <div className="ms-welcome">
+        <Header logo="assets/logo-filled.png" title={this.props.title} message="Welcome" />
+      </div>
+    );
+  }
+}
