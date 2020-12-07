894c382948bf2a842f603dde35b40ee0746ad480
Sandun Tharaka <spstharaka@gmail.com>
2020-12-07 23:32:45 +0530

    Add production manifest file

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -11,7 +11,7 @@ initializeIcons();
 
 let isOfficeInitialized = false;
 
-const title = "Contoso Task Pane Add-in";
+const title = "Placekey Excel Add-in";
 
 const render = Component => {
   ReactDOM.render(
