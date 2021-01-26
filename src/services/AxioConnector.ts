e6fef7d0670615d9dda9975b6daacc5b5f6cf969
Sandun Tharaka <spstharaka@gmail.com>
2021-01-27 00:29:21 +0530

    Pass the client header to axios request

--- src/services/AxioConnector.ts
+++ src/services/AxioConnector.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { base_url } from '../utils/APIUrls';
-//import { addin_version } from '../utils/Config';
+import { addin_version } from '../utils/Config';
 
 const axioConnectorInstance = axios.create({
   baseURL: base_url,
@@ -11,6 +11,6 @@ const axioConnectorInstance = axios.create({
 });
 
 // getting an error when setting user agent header via the axios - 'Refused to set unsafe header "User-Agent"'
-//axioConnectorInstance.defaults.headers['User-Agent'] = `placekey-excel/${addin_version}`; 
+axioConnectorInstance.defaults.headers['X-PK-App'] = `placekey-excel/${addin_version}`; 
 
 export default axioConnectorInstance;
